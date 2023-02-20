cff212bc0e2a4108b7f9789bc58496d24a9089f3
Nathanael Roviery <roviery@gmail.com>
2023-02-20 11:27:09 +0700

    feat: add CORS middleware

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/roviery/catetin-api/routes"
+import (
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/roviery/catetin-api/routes"
+)
 
 func main() {
 	e := routes.Init()
+	e.Use(middleware.CORS())
 	e.Logger.Fatal(e.Start("localhost:8080"))
 }
